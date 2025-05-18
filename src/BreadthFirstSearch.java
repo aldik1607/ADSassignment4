1ec37e74605c7a65b2ec804e91615221ee9ae93f
aldik1607 <aldiyar.tandau2007@gmail.com>
2025-05-18 12:08:24 +0500

    added weighted bfs

---
+++ src/BreadthFirstSearch.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class BreadthFirstSearch<V> extends Search<V> {
+    private Map<Vertex<V>, Vertex<V>> edgeTo = new HashMap<>();
+    private Set<Vertex<V>> marked = new HashSet<>();
+    private Vertex<V> source;
+
+    public BreadthFirstSearch(WeightedGraph<V> graph, Vertex<V> source) {
+        this.source = source;
+        bfs(graph, source);
+    }
+
+    private void bfs(WeightedGraph<V> graph, Vertex<V> s) {
+        Queue<Vertex<V>> queue = new LinkedList<>();
+        marked.add(s);
+        queue.add(s);
+
+        while (!queue.isEmpty()) {
+            Vertex<V> v = queue.poll();
+            for (Vertex<V> w : v.getAdjacentVertices().keySet()) {
+                if (!marked.contains(w)) {
+                    edgeTo.put(w, v);
+                    marked.add(w);
+                    queue.add(w);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean hasPathTo(Vertex<V> vertex) {
+        return marked.contains(vertex);
+    }
+
+    @Override
+    public List<Vertex<V>> pathTo(Vertex<V> vertex) {
+        if (!hasPathTo(vertex)) return null;
+        List<Vertex<V>> path = new ArrayList<>();
+        for (Vertex<V> x = vertex; edgeTo.containsKey(x); x = edgeTo.get(x)) {
+            path.add(x);
+        }
+        path.add(edgeTo.containsKey(vertex) ? edgeTo.get(vertex) : vertex); // добавляем начальную вершину
+        Collections.reverse(path);
+        return path;
+    }
+
+}
