774b6b9ebb2f1584fb303098b841a20cce5787a3
aldik1607 <aldiyar.tandau2007@gmail.com>
2025-05-18 12:08:40 +0500

    added weighted main test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Vertex<String> A = new Vertex<>("A");
+        Vertex<String> B = new Vertex<>("B");
+        Vertex<String> C = new Vertex<>("C");
+
+        WeightedGraph<String> graph = new WeightedGraph<>();
+        graph.addVertex(A);
+        graph.addVertex(B);
+        graph.addVertex(C);
+
+        graph.addEdge(A, B, 5.0);
+        graph.addEdge(B, C, 2.0);
+        graph.addEdge(A, C, 10.0);
+
+        DijkstraSearch<String> dijkstra = new DijkstraSearch<>(graph, A);
+        BreadthFirstSearch<String> bfs = new BreadthFirstSearch<>(graph, A);
+
+        System.out.println("Dijkstra path from A to C: " + dijkstra.pathTo(C));
+        System.out.println("BFS path from A to C: " + bfs.pathTo(C));
     }
 }
